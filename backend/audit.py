0714e450320bfa9c9461fa52dab5265ad9acfccc
griffWork <griffinjohnson1414@gmail.com>
2026-07-02 23:29:40 -0700

    Added the audit logic

---
+++ backend/audit.py
@@ -0,0 +1,26 @@
+import sqlite3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_PATH = os.getenv('DATABASE_PATH', '../database/mfa.db')
+
+# This audit log will collect all the important secruity events that occur in the application.
+def log_event(event_type, success, user_id=None, ip_address=None):
+    try:
+        conn = sqlite3.connect(DATABASE_PATH)
+        cursor = conn.cursor()
+
+        # Parameterized query to prevent SQL injection attacks. Input goes through the query parameters instead of being directly concatenated into the SQL string.
+        cursor.execute('''
+            INSERT INTO audit_log (user_id, event_type, ip_address, success)
+            VALUES (?, ?, ?, ?)
+        ''', (user_id, event_type, ip_address, 1 if success else 0))
+        
+        conn.commit()
+        conn.close()
+        
+    # Print logging failures.
+    except Exception as e:
+        print(f"Audit log error: {e}")
